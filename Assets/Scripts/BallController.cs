9850860209bdd50cabda80b9b1068499028b0ebd
mitsuru776 <pict7pict@gmail.com>
2020-10-25 16:33:00 +0900

    first
    first

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public GameObject particle;
+
+    [SerializeField]
+    private float speed;
+    bool started;
+    bool gameOver;
+    Rigidbody rb;
+
+    void Awake(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        started = false;
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!started){
+            if(Input.GetMouseButtonDown(0)){
+                Debug.Log("de, world!");
+                rb.velocity = new Vector3(speed,0,0);
+                started = true;
+            }
+        }
+
+        if(!Physics.Raycast(transform.position,Vector3.down,1f)){
+            gameOver = true;
+            rb.velocity = new Vector3(0,-25f,0);
+            Camera.main.GetComponent<CameraFollow>().gameOver = true;
+        }
+
+        if(Input.GetMouseButtonDown(0) && !gameOver){
+            SwitchDirection();
+            Debug.Log("Pressed primary button.");
+        }
+    }
+
+    void SwitchDirection(){
+        if(rb.velocity.z >0){
+            rb.velocity = new Vector3(speed,0,0);
+            Debug.Log("ZZZZZZZZZZZZZZZZZz");
+        }else if(rb.velocity.x > 0){
+            rb.velocity = new Vector3(0,0,speed);
+            Debug.Log("XXXXXXXXXXXXXXXXx");
+        }
+
+    }
+
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag == "Diamond"){
+            GameObject part = Instantiate(particle, col.gameObject.transform.position,Quaternion.identity) as GameObject;
+            Destroy(col.gameObject);
+            Destroy(part,1f);
+
+        }
+    }
+}
